4182674091cadeafc7107eb936aa8905d3c5e66e
robbyishere <jkoh1295@gmail.com>
2021-08-04 19:37:23 -0700

    Finished equationCompute and rightShift functions

--- Hash.v
+++ Hash.v
@@ -68,6 +68,7 @@ module Hash(
 			    binaryAddition[3] =  block[j][2047-(32*(k-16))-:32];
 			    //$display(k," binaryAddition[1]:", "%b", binaryAddition[1]);
 			    //$display(k," binaryAddition[3]:", "%b", binaryAddition[3]);
+			    binaryAddition[0] = equationCompute(1,17,19,10,block[j][(2047-(32*(k-2)))-:32]);
 		    end
 	        end
                 $display("Block 0:", "%b", block[0]);
@@ -77,4 +78,63 @@ module Hash(
         end
         status = 1;
         end
+	function [31:0] equationCompute;
+		input computeType;
+		input [5:0] shiftVal1, shiftVal2, shiftVal3;
+		input [31:0] word;
+		reg [31:0] data [3:0];
+		begin
+			//$display(computeType," ", shiftVal1," ", shiftVal2, " ", shiftVal3, " ", "%b", word);
+			data[0] = rightShift(shiftVal1, word);
+			data[1] = rightShift(shiftVal2, word);
+			if(computeType == 0)begin
+				data[2] = word>>shiftVal3; //Normal Shift
+			end
+			else begin
+				data[2] = rightShift(shiftVal3, word);
+			end
+			equationCompute = data[0]^data[1]^data[2]; //XOR
+		end
+	endfunction
+	function [31:0] rightShift;
+		input [5:0] shiftVal;
+		input [31:0] word;
+		reg [31:0] copy;
+		begin
+			copy = 0;
+			case(shiftVal)
+				7:begin
+					copy[31:32-7] = word[7-1:0];
+				end
+				18:begin
+					copy[31:32-18] = word[18-1:0];
+				end
+				17:begin
+					copy[31:32-17] = word[17-1:0];
+				end
+				19:begin
+					copy[31:32-19] = word[19-1:0];
+				end
+				2:begin
+					copy[31:32-2] = word[2-1:0];
+				end
+				13:begin
+					copy[31:32-13] = word[13-1:0];
+				end
+				22:begin
+					copy[31:32-22] = word[22-1:0];
+				end
+				6:begin
+					copy[31:32-6] = word[6-1:0];
+				end
+				11:begin
+					copy[31:32-11] = word[11-1:0];
+				end
+				25:begin
+					copy[31:32-25] = word[25-1:0];
+				end
+			endcase
+			rightShift = (word>>shiftVal)+copy;
+		end
+	endfunction
 endmodule
