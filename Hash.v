71e65ee6b02d5eb1860acd4c7997a658392a108b
robbyishere <jkoh1295@gmail.com>
2021-08-04 19:58:27 -0700

    Blocks are now filled, removed unused prints

--- Hash.v
+++ Hash.v
@@ -39,12 +39,6 @@ module Hash(
         end
         else begin
             msg[1023:384] = header[639:0];
-            /*
-            $display("Header         : ", "%b", header);
-            $display("msg          : ", "%b", msg);
-            $display("Hash Values    : ", "%b", initialHashValues);
-            $display("Constant Values: ", "%b", constantValues);
-            */
             for(i=0; i<1; i=i+1) begin
                 if(i==0) begin
                     msg[383] = 1; //Separator
@@ -58,17 +52,16 @@ module Hash(
                     msg[8] = 1; //Message Length
                     blockCount = 1;
                 end
-                $display("msg          : ", "%b", msg);
                 //Split message into blocks based on blockCount
 	        for(j=0; j<blockCount; j=j+1) begin
 		    block[j][2047:1536] = msg[((512*blockCount)-(512*j))-1-:512];
-		    $display("Block: ", j);
+		    //Fill block
 		    for(k=16; k<64; k=k+1)begin
+			    binaryAddition[0] = equationCompute(0,17,19,10,block[j][(2047-(32*(k-2)))-:32]);
 			    binaryAddition[1] =  block[j][2047-(32*(k-7))-:32];
+			    binaryAddition[2] = equationCompute(0,7,18,3,block[j][(2047-(32*(k-15)))-:32]);
 			    binaryAddition[3] =  block[j][2047-(32*(k-16))-:32];
-			    //$display(k," binaryAddition[1]:", "%b", binaryAddition[1]);
-			    //$display(k," binaryAddition[3]:", "%b", binaryAddition[3]);
-			    binaryAddition[0] = equationCompute(1,17,19,10,block[j][(2047-(32*(k-2)))-:32]);
+			    block[j][2047-(32*k)-:32] = binaryAddition[0]+binaryAddition[1]+binaryAddition[2]+binaryAddition[3];
 		    end
 	        end
                 $display("Block 0:", "%b", block[0]);
@@ -84,7 +77,6 @@ module Hash(
 		input [31:0] word;
 		reg [31:0] data [3:0];
 		begin
-			//$display(computeType," ", shiftVal1," ", shiftVal2, " ", shiftVal3, " ", "%b", word);
 			data[0] = rightShift(shiftVal1, word);
 			data[1] = rightShift(shiftVal2, word);
 			if(computeType == 0)begin
