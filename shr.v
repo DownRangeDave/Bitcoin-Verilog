7da9d3726d619402e89a7833b4e7787841ff1ed2
DownRangeDave <42917669+DownRangeDave@users.noreply.github.com>
2021-08-09 00:16:56 -0600

    New old files updated
    improved and faster than original

---
+++ shr.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/21/2021 01:31:53 PM
+// Design Name: 
+// Module Name: shr
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module shr
+#(parameter range=4)(
+    
+    input [31:0] data,
+    output [31:0] shiftedData
+    );
+    integer i;
+    assign shiftedData =  data>>range;
+ 
+endmodule
